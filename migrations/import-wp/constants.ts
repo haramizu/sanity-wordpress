3e7b558512737c7406482939729a7388a389d391
Shinichi Haramizu <shinichi@haramizu.jp>
2025-09-13 10:35:04 +0900

    add migration scripts

---
+++ migrations/import-wp/constants.ts
@@ -0,0 +1,3 @@
+// Replace this with your WordPress site's WP-JSON REST API URL
+export const BASE_URL = `https://public-api.wordpress.com/rest/v1.1/sites/haramizujp.wordpress.com`
+export const PER_PAGE = 100
