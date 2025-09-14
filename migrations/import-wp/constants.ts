7c086ef73a6be2cecf8b44d050b3468ce36d75b9
Shinichi Haramizu <shinichi@haramizu.jp>
2025-09-14 12:26:59 +0900

    add DataTypes

--- migrations/import-wp/constants.ts
+++ migrations/import-wp/constants.ts
@@ -1,3 +1,13 @@
+import type {SanitySchemaType, WordPressDataType} from './types'
+
 // Replace this with your WordPress site's WP-JSON REST API URL
 export const BASE_URL = `https://public-api.wordpress.com/wp/v2/sites/haramizujp.wordpress.com`
 export const PER_PAGE = 100
+
+export const WP_TYPE_TO_SANITY_SCHEMA_TYPE: Record<WordPressDataType, SanitySchemaType> = {
+  categories: 'category',
+  posts: 'post',
+  pages: 'page',
+  tags: 'tag',
+  users: 'author',
+}
