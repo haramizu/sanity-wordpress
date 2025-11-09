26fd84ff39843cf0fff6aef6b2941bf6fac8c50c
Shinichi Haramizu <shinichi@haramizu.jp>
2025-11-09 15:08:26 +0900

    add htmlToBlockContent

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -5,7 +5,32 @@ import {pageType} from './pageType'
 import {postType} from './postType'
 import {tagType} from './tagType'
 import {portableTextType} from './portableTextType'
+import { SchemaTypeDefinition } from 'sanity'
 
+// Import built-in Sanity image types that are required for the image type
+const sanityImageHotspot: SchemaTypeDefinition = {
+  name: 'sanity.imageHotspot',
+  title: 'Image Hotspot',
+  type: 'object',
+  fields: [
+    {name: 'x', type: 'number'},
+    {name: 'y', type: 'number'},
+    {name: 'height', type: 'number'},
+    {name: 'width', type: 'number'},
+  ],
+}
+
+const sanityImageCrop: SchemaTypeDefinition = {
+  name: 'sanity.imageCrop',
+  title: 'Image Crop',
+  type: 'object',
+  fields: [
+    {name: 'top', type: 'number'},
+    {name: 'bottom', type: 'number'},
+    {name: 'left', type: 'number'},
+    {name: 'right', type: 'number'},
+  ],
+}
 export const schemaTypes = [
   authorType,
   categoryType,
@@ -14,4 +39,6 @@ export const schemaTypes = [
   tagType,
   externalImageType,
   portableTextType,
+  sanityImageHotspot,
+  sanityImageCrop,
 ]
